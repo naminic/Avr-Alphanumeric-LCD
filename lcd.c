34c676cd6dbf207968305ebff617b243a54a96a0
naminic <info@naminic.com>
2013-10-25 09:58:05 +0330

    source code
    upload source

---
+++ lcd.c
@@ -0,0 +1,73 @@
+/*****************************************************
+Chip type               : ATmega32
+Program type            : Application
+AVR Core Clock frequency: 8.000000 MHz
+Memory model            : Small
+External RAM size       : 0
+Data Stack size         : 512
+
+*****************************************************
+
+                WWW.NAMINIC.COM
+
+*****************************************************/
+
+#include <mega32.h>
+
+// Alphanumeric LCD functions
+#include <alcd.h>
+//#include <alcd_ks0073.h>
+
+// Declare your global variables here
+
+void main(void)
+{
+// Declare your local variables here
+
+// Input/Output Ports initialization
+// Port A initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
+PORTA=0x00;
+DDRA=0x00;
+
+// Port B initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
+PORTB=0x00;
+DDRB=0x00;
+
+// Port C initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
+PORTC=0x00;
+DDRC=0x00;
+
+// Port D initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
+PORTD=0x00;
+DDRD=0x00;
+
+// Alphanumeric LCD initialization
+// Connections are specified in the
+// Project|Configure|C Compiler|Libraries|Alphanumeric LCD menu:
+// RS - PORTD Bit 0
+// RD - PORTD Bit 1
+// EN - PORTD Bit 2
+// D4 - PORTD Bit 4
+// D5 - PORTD Bit 5
+// D6 - PORTD Bit 6
+// D7 - PORTD Bit 7
+// Characters/line: 16
+lcd_init(16);
+
+lcd_clear();
+
+lcd_gotoxy(0,0);
+
+lcd_putsf("WWW.NAMINIC.COM");
+
+while (1);
+
+}
